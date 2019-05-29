8d4b5189da28fd3e57c533eb24d80f25856c9101
pdronenko <pdronenko@ya.ru>
2019-05-29 15:01:51 +0300

    init project

--- src/Example.js
+++ src/Example.js
@@ -5,13 +5,13 @@ export default class Example {
 
   init() {
     this.element.innerHTML = `
-      <div class="jumbotron">
-        <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </div>
+      <main role="main" class="container">
+        <div class="jumbotron">
+          <h1>Navbar example</h1>
+          <p class="lead">This example is a quick exercise to illustrate how the top-aligned navbar works. As you scroll, this navbar remains in its original position and moves with the rest of the page.</p>
+          <a class="btn btn-lg btn-primary" href="../../components/navbar/" role="button">View navbar docs</a>
+        </div>
+      </main>
     `;
   }
 }
