9c3905d59f7a0e4c0cc5e82d92bff052a0d55fb4
pdronenko <pdronenko@ya.ru>
2019-05-29 14:33:43 +0300

    init project

--- src/Example.js
+++ src/Example.js
@@ -4,7 +4,14 @@ export default class Example {
   }
 
   init() {
-    this.element.textContent = 'hello, world!';
-    console.log('ehu!');
+    this.element.innerHTML = `
+      <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </div>
+    `;
   }
 }
