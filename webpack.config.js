159666d4cf8a9e40f21379767325f4a043407524
pdronenko <pdronenko@ya.ru>
2019-06-08 11:03:18 +0300

    add dev-server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/'),
+  },
+  devtool: 'source-map',
   devServer: {
     contentBase: './src',
+    watchContentBase: true,
   },
   module: {
     rules: [
