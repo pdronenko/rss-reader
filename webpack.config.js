c729978f7df22a8664aa7a8a5a20c249d313f4e9
pdronenko <pdronenko@ya.ru>
2019-06-12 16:40:02 +0300

    add babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: [
-    './src/index.js',
-  ],
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist/'),
